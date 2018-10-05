b6565ed85ac5469d9edf515db7eb0637a932ca74
Wesley\BTSSNEC <adouki.wesley@hotmail.com>
2018-10-05 07:30:12 +0200

    Rajout du php pour faire des blocs de page ! Et création de la page admin

---
+++ WEB/admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html id="admin">
+<title>Admin</title>
+<meta charset="UTF-8>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<?php include("header.php"); ?>
+
+<form method="post" action="cible.php">
+ 
+<p>
+	<input type="text" name="prenom" />
+	<input type="text" name="nom" />
+	<input type="submit" value="Valider" />
+</p> 
+</form>
--- WEB/index.php
+++ WEB/index.php
@@ -5,18 +5,8 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
 <body>
 
-<!-- Navbar (sit on top) -->
-<div class="w3-top">
-  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
-    <a href="#home" class="w3-bar-item w3-button">Wes PROD</a>
-    <!-- Float links to the right. Hide them on small screens -->
-    <div class="w3-right w3-hide-small">
-      <a href="#projects" class="w3-bar-item w3-button">Jeux</a>
-      <a href="#about" class="w3-bar-item w3-button">A propos</a>
-      <a href="#contact" class="w3-bar-item w3-button">Nous Contacter</a>
-    </div>
-  </div>
-</div>
+<?php include("header.php"); ?>
+
 
 
 <!-- Page content -->
