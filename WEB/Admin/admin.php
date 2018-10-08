cc4df4a5cffffb274955a5bfa848abb394671633
Wesley\BTSSNEC <adouki.wesley@hotmail.com>
2018-10-08 08:53:26 +0200

    Formulaire d'Admin pour classe !

--- WEB/Admin/admin.php
+++ WEB/Admin/admin.php
@@ -3,19 +3,39 @@
 <title>Admin</title>
 <meta charset="UTF-8>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" type="text/css" href="style.css">
 <?php include($_SERVER["DOCUMENT_ROOT"].'../Outils/header.php'); ?>
 
 <body>
-<div id="formulaire" position="200px">
-	<form method="post" action="cible.php" ng-submit="login()">
- 
-	<p>
-		<label for="nom">Prenom: </label>
-		<input type="text" name="prenom" /><br>
-		<label for="nom">Nom:</label>
-		<input type="text" name="nom" /><br>
-		<input type="submit" value="Valider" />
-	</p> 
+<br><br><br>
+<div id="formulaire">
+	<form method="post" action="cible.php" ng-submit="login()"> 
+		<label for="classe">Classe: </label><input type="text" name="classe" id="classe" required /><span id="missClass"></span> <br>
+		<label for="hp">HP: </label><input type="text" name="hp" required /><br>
+		<label for="mp">MP:</label><input type="text" name="mp" required /><br>
+		<label for="atk">ATK:</label><input type="text" name="atk" required /><br>
+		<label for="def">DEF:</label><input type="text" name="def" required /><br>
+		<label for="mat">MAT:</label><input type="text" name="mat" required /><br>
+		<label for="mdf">MDF:</label><input type="text" name="mdf" required /><br>
+		<label for="agi">AGI:</label><input type="text" name="agi" required /><br>
+		<label for="luk">LUK:</label><input type="text" name="luk" required /><br>	
+		<input type="submit" value="Valider" id="bouton_validation" />	 
 	</form>
 </div>
+
+<script>
+	var formValid = document.getElementById('bouton_validation');
+	var classe = document.getElementById('classe')
+	var missClass = document.getElementById('missClass')
+	
+	formValid.addEventListener('click', validation);
+	
+	function validation(event){
+		if (classe.validity.valueMissing){
+			event.preventDefault();
+			missClass.textContent = 'Nom manquant';
+			missClass.style.color = 'red';
+		}
+	}
+</script>
 </body>
--- WEB/Admin/cible.php
+++ WEB/Admin/cible.php
@@ -1,6 +1,41 @@
 <!DOCTYPE html>
-<p>Bonjour !</p>
+<p> Tu as choisi la classe <?php echo $_POST['classe']; ?> !</p>
 
-<p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['prenom']; ?> !</p>
+<?php
 
-<p>Si tu veux changer de prénom, <a href="formulaire.php">clique ici</a> pour revenir à la page formulaire.php.</p>
+$nom = $_POST['classe'];
+$hp = $_POST['hp'];
+$mp = $_POST['mp'];
+$atk = $_POST['atk'];
+$def = $_POST['def'];
+$mat = $_POST['mat'];
+$mdf = $_POST['mdf'];
+$agi = $_POST['agi'];
+$luk = $_POST['luk'];
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=dissidious_db;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+$req = $bdd->prepare('INSERT INTO classe(nom, hp, mp, atk, def, mat, mdf, agi, luk) VALUES(:nom, :hp, :mp, :atk, :def, :mat, :mdf, :agi, :luk)');
+$req->execute(array(
+	'nom' => $nom,
+	'hp' => $hp,
+	'mp' => $mp,
+	'atk' => $atk,
+	'def' => $def,
+	'mat' => $mat,
+	'mdf' => $mdf,
+	'agi' => $agi,
+	'luk' => $luk	
+	));
+
+
+echo 'La classe a bien été ajouté !';
+?>
+
