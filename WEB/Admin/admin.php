83a317f24b0a692c13fce0598a60ff7dcf472a39
Wesley\BTSSNEC <adouki.wesley@hotmail.com>
2018-10-14 16:20:45 +0200

    Correction connection SQL local à serveur

--- WEB/Admin/admin.php
+++ WEB/Admin/admin.php
@@ -65,12 +65,12 @@
 	</tr>
 <?php
 
-$mysqli = new mysqli('localhost', 'root', '', 'dissidious_db');
+$mysqli = new mysqli('localhost', 'dissidious', 'revenge', 'dissidious_db');
         $mysqli->set_charset("utf8");
         $requete = 'SELECT * FROM classe';
         $resultat = $mysqli->query($requete);
         while ($ligne = $resultat->fetch_assoc()) {
-            echo '</td><td>'.$ligne['nom'].'</td><td>'.$ligne['type'].'</td><td>'.$ligne['hp'].'</td><td>'.$ligne['mp'].'</td><td>'
+            echo '<tr><td>'.$ligne['nom'].'</td><td>'.$ligne['type'].'</td><td>'.$ligne['hp'].'</td><td>'.$ligne['mp'].'</td><td>'
 			.$ligne['atk'].'</td><td>'.$ligne['def'].'</td><td>'.$ligne['mat'].'</td><td>'.$ligne['mdf'].'</td><td>'.$ligne['agi'].'</td><td>'
 			.$ligne['luk'].'</td><td>'.$ligne['total'].'</td></tr>';            
         }
