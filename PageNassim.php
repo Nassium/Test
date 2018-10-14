c5df0f252e64e2d2637caaf32f4d263789a1e214
DESKTOP-L54D9H0\NASSIM <bekkouce.nassim@gmail.com>
2018-10-14 23:51:21 +0200

    Ajout de la PageNassim.php avec ajout d'une image dans "img"et suppresion de la PageNassim.html

---
+++ PageNassim.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html id="home">
+<title>Accueil</title>
+<meta charset="UTF-8">
+<?php include($_SERVER["DOCUMENT_ROOT"].'/Outils/header.php'); ?>
+<body>
+
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+
+  <!-- Project Section -->
+    <div class="w3-container w3-padding-32" id="projects">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Page de Nassim</h3>
+  </div>
+
+ <!-- Menu de navigation du site -->
+<ul class="navbar">
+  <li><a href="index.php">Home page</a>
+</ul>
+
+  <style type="text/css">
+  body {
+    color: black;
+    background-color: #D3D3D3 }
+  </style>
+
+<img src="img/ronaldo.png" align="left"/>
+</div> 
+
+
+</body>
+</html>
