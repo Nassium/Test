c29aea56351c0e4f152cf109171bf13325ad2666
Wesley\BTSSNEC <adouki.wesley@hotmail.com>
2018-10-21 13:56:44 +0200

    Correction des accents qui passaient pas dans le menu

--- Outils/menu_index.php
+++ Outils/menu_index.php
@@ -1,16 +1,17 @@
+ï»¿<!DOCTYPE html>
 <!--<link rel="stylesheet" type="text/css" href="/style.css">-->
 <ul id="menu">
 	<li><a href="#">Children Of memories</a>
 		<ul>
 			<li><a href="#">Prologue</a></li>
-			<li><a href="#">Scénario</a></li>
+			<li><a href="#">ScÃ©nario</a></li>
 			<li><a href="#">Personnages</a></li>
 			<li><a href="#">Classes</a></li>
 		</ul>
 	</li>
 	<li><a href="#">Dissidious Revenge</a>
 		<ul>
-			<li><a href="/Scenario/Dissidious/index_scenario.php">Scénario</a></li>
+			<li><a href="/Scenario/Dissidious/index_scenario.php">ScÃ©nario</a></li>
 			<li><a href="/Personnages/Dissidious/index_persos.php">Personnages</a></li>
 			<li><a href="/Classe/Dissidious/index_classe.php">Classes</a></li>
 			<li><a href="#">Astuces</a></li>
