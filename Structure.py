e1c5f6f1af6495856ccb0899bfc4fba4e19a846f
Wesley\BTSSNEC <adouki.wesley@hotmail.com>
2019-02-19 07:33:59 +0100

    Création de la classe contenant les valeurs statiques du Classeur

---
+++ Structure.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+# La classe représentant les parties statiques du classeur
+class Structure:
+	
+	def __init__(self):
+		# Création du classeur 
+		workbook = xlsxwriter.Workbook('CRA_Fevrier_2019.xlsx')
+		# Création de la feuille et nommage de celle-ci
+		worksheet = workbook.add_worksheet('Feuille1')
+		
+	def remplir_classeur(self, cellule, chaine, format):
+		self.cellule = cellule
+		self.chaine = chaine
+		self.format = format 
+		worksheet.write(cellule, chaine, format)
